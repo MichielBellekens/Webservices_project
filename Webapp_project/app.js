186b99ed356400ff921994f0b65ff720a5b180f6
MichielBellekens <michiel.bellekens@hotmail.com>
2016-10-10 21:24:06 +0200

    login with password hashing and creation of new accounts

--- Webapp_project/app.js
+++ Webapp_project/app.js
@@ -1,23 +1,27 @@
-//include express to make templates possible
-var expr =  require("express");
-//include path to be able to get the current dir
-var path = require("path");
-//parser to get data from the inputfield of the view using post --> see express docs about req.body
-var bodyParser = require('body-parser')
-//Creates an Express application.
-var app = expr();
+/*
+    TO DO
+        CHECK TO DO'S IN THE CODE
+        CLEAN UP THE CODE
+        REMOVE ALL OF THE CONSOLE.LOG()
+        cREATE HTTPS INSTEAD OF HTTP
+        SECURITY ANDCERTIFICATES
+        CHECK IF VIEW ARE VALID
+*/
 
-//set the view engine to ejs for the app express application
-app.set("view engine", "ejs");
-//Point the views setting to the dir that contains all the project views
-app.set("views", path.join(__dirname, 'Views'));
-//parser to get data from the inputfield of the view using post --> see express docs about req.body
+var expr =  require("express");                     //include express to make templates possible
+var path = require("path");                         //include path to be able to get the current dir
+var bodyParser = require('body-parser');            //parser to get data from the inputfield of the view using post --> see express docs about req.body
+var app = expr();                                   //Creates an Express application.
+var hashing = require('password-hash-and-salt');    //include the hashin lib to hash and verify the passwords of the users
+app.set("view engine", "ejs");                      //set the view engine to ejs for the app express application
+app.set("views", path.join(__dirname, 'Views'));    //Point the views setting to the dir that contains all the project views
 app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
+//listen on an 1337 for all communication
 app.listen(1337,function(){
     console.log('Ready on port 1337');
 });
 
-//values to parsed database elements
+//create connection to database
 var mysql      = require('mysql');
 var connection = mysql.createConnection({
     host     : '127.0.0.1',
@@ -28,22 +32,22 @@ var connection = mysql.createConnection({
 
 //a variable to store the currently logged in users activities for all of the categories
 var activities;
+//variable to store the currently logged in user' id
 var current_user_id;
-//load_activities();
+//common footer for all the pages
+var footer = "2016 Michiel Bellekens webapplications&services @ Thomas More Denayer Sint-Katelijne-Waver";
+
+//function to load all the activities of the currently logged in user into the local variable activities
 function load_activities(userid)
 {
-    console.log("entering loading function");
+    console.log("Loading all the activities ...");
     console.log(current_user_id);
     connection.query('SELECT * FROM activities WHERE userID='+userid,function(err,rows){
         if(err) throw err;
         console.log(rows);
-        create_local_variable(rows);
+        activities = rows;
     });
 }
-function create_local_variable(data) {
-    activities = data;
-}
-
 
 //All the values for the tabs
 var tabs = [
@@ -54,17 +58,18 @@ var tabs = [
     {link: "add", text : "Add new item"}
 ];
 
-var footer = "2016 Michiel Bellekens webapplications&services @ Thomas More Denayer Sint-Katelijne-Waver";
+//the function to render a page when a get request is send to the root page --> the parameters are passed to fill the variable spots inside the view pages
 app.get('/', function(req,res){
     res.render('Index',{
         title : "To do lists home page",
-        cur_user:current_user_id,
+        cur_user: current_user_id,
         tab : tabs,
         message : " This page keeps track of all of your To do list from all your activities",
         footer: footer
     });
 });
 
+//function that renders the page after the school page is requested --> only accessible when the activiteis are loaded
 app.get('/school', function(req,res){
     if (activities == null)
     {
@@ -83,6 +88,7 @@ app.get('/school', function(req,res){
     }
 });
 
+//render the spare time tab
 app.get('/spare_time', function(req,res){
     if (activities == null)
     {
@@ -101,6 +107,7 @@ app.get('/spare_time', function(req,res){
     }
 });
 
+//render the others page
 app.get('/others', function(req,res){
     if (activities == null)
     {
@@ -119,7 +126,7 @@ app.get('/others', function(req,res){
     }
 });
 
-
+//render the page to add an activity
 app.get('/add', function(req,res){
     if (activities == null)
     {
@@ -135,6 +142,7 @@ app.get('/add', function(req,res){
     }
 })
 
+//render the page when the user wants to edit an item --> the id of the item that needs to be edited is passed
 app.get('/edit', function(req,res){
     res.render('Tabs',{
         title : "To do lists " +req.param("current_cat"),
@@ -146,6 +154,7 @@ app.get('/edit', function(req,res){
     });
 });
 
+//is run when the delete link is pressed --> the get param are used to determine which element needs to be deleted
 app.get('/delete', function(req,res){
     console.log("entering the delete post");
     var querystring = 'DELETE FROM activities WHERE ID=' + req.param("id") + ' AND category= "' + req.param("current_cat")+'"';
@@ -159,7 +168,7 @@ app.get('/delete', function(req,res){
     });
 });
 
-
+//save the edited information to the database and reload the local variable activities
 app.post('/edit', function(req,res){
     console.log("entering the edit post");
     var id = req.body.ID;
@@ -181,6 +190,7 @@ app.post('/edit', function(req,res){
         });
 });
 
+//save the newly added value to the database and reload the local variable activities
 app.post('/addnew', function(req,res){
     console.log("entering the new post");
     var cat = req.body.Category_select;
@@ -197,32 +207,45 @@ app.post('/addnew', function(req,res){
     });
 });
 
+//log in and check the information provide by the user in the log in fields
 app.post('/login', function(req,res){
     var mail = req.body.email;
     var passw = req.body.password;
-    var querystring= "Select * FROM users where Mail = '"+ mail + "' AND password='" + passw +"'";
+    var querystring= "Select * FROM users where Mail = '"+ mail + "'";
     console.log(querystring);
     connection.query(querystring,function(err,rows){
-        if(rows.length != 1)
+        if(rows.length == 1)
         {
-            res.redirect('/');
+            hashing(req.body.password).verifyAgainst(rows[0].Password, function(error, verified) {
+                if(error)
+                    throw new Error('Failed to verify the password');
+                if(!verified) {
+                    console.log("Not the correct password");
+                }
+                else
+                {
+                    console.log("successfully logged in");
+                    load_activities(rows[0].ID);
+                    current_user_id = rows[0].ID;
+                    res.redirect('/school');
+                }
+            });
         }
         else
         {
-            console.log("successfully logged in");
-            load_activities(rows[0].ID);
-            current_user_id = rows[0].ID;
-            res.redirect('/school');
+            res.redirect('/');
         }
     });
 });
 
+//perform all actions to logout the user
 app.get('/logout', function(req,res){
     activities = null;
     current_user_id =  null;
     res.redirect('/');
 });
 
+//render the page to create a new account
 app.get('/create_account', function(req,res){
     res.render('create_account',{
         title : "Create a new account",
@@ -231,27 +254,41 @@ app.get('/create_account', function(req,res){
     });
 });
 
+//Saves the new account --> checks if the mail is allready used in another account
+//TO DO ==>  CHECK IF NONE OF THE FIELDS ARE EMPTY
 app.post('/create_account', function(req,res){
     var mail = req.body.Mailaddress;
-    var passw = req.body.passwd;
-    var querystring= "Select * FROM users where Mail = '"+ mail + "'";
-    console.log(querystring);
-    connection.query(querystring,function(err,rows){
-        console.log(rows.length);
-        if(rows.length == 0)
-        {
-            console.log("Entering creation loop");
-            querystring = "INSERT INTO users (Mail, Password) VALUES ('"+mail+"','"+passw+"')";
-            connection.query(querystring, function(err, rows)
+    hashing(req.body.passwd).hash(function (error, hash) {
+        if (error)
+            throw new Error('Hashing produced an error');
+
+        var querystring= "Select * FROM users where Mail = '"+ mail + "'";
+        console.log(querystring);
+        connection.query(querystring,function(err,rows){
+            console.log(rows.length);
+            if(rows.length == 0)
             {
-                if(err) throw err;
-                console.log("adding new account");
-            });
-        }
-        else
-        {
-            console.log("The mail is already used");
-            res.redirect('/');
-        }
+                console.log("Entering creation loop");
+                querystring = "INSERT INTO users (Mail, Password) VALUES ('"+mail+"','"+hash+"')";
+                connection.query(querystring, function(err, rows)
+                {
+                    if(err) throw err;
+
+                    querystring = "SELECT * FROM users where Mail='"+mail+"'";
+                    connection.query(querystring, function (err, rows) {
+                        if(err) throw err;
+
+                        current_user_id = rows[0].ID;
+                        res.redirect('/school');
+                    });
+                    console.log("adding new account");
+                });
+            }
+            else
+            {
+                console.log("The mail is already used");
+                res.redirect('/');
+            }
+        });
     });
 });
